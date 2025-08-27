50628e2ca72e89a50df5083ca3d23e6d62df025a
Claudio <claudioclaudio91@gmail.com>
2025-08-27 17:31:29 -0500

    Added Prettier, parameterized map name and floor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
-import Map from './components/MapLeaflet'
-import './App.css'
+import Map from "./components/MapLeaflet";
+import "./App.css";
+import { useState } from "react";
+import type { Floor } from "./types/MapTypes";
 
 function App() {
-  return (
-    <Map />
-  )
+    const [floor, setFloor] = useState<Floor>("basement");
+
+    return (
+        <>
+            <div className="flex flex-row gap-2">
+                <button
+                    className="cursor-pointer"
+                    onClick={() => setFloor("basement")}
+                >
+                    Basement
+                </button>
+                <button
+                    className="cursor-pointer"
+                    onClick={() => setFloor("first")}
+                >
+                    First
+                </button>
+                <button
+                    className="cursor-pointer"
+                    onClick={() => setFloor("second")}
+                >
+                    Second
+                </button>
+                <button
+                    className="cursor-pointer"
+                    onClick={() => setFloor("roof")}
+                >
+                    Roof
+                </button>
+            </div>
+            <Map mapName="chalet" floor={floor} />
+        </>
+    );
 }
 
-export default App
+export default App;
