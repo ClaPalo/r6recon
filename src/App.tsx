a21301240b92461e5bd1805e24d3314bef851933
Claudio <claudioclaudio91@gmail.com>
2025-09-05 16:44:08 -0500

    Added modal window to choose the map

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,27 @@
-import Map from "./components/MapLeaflet";
-import "./App.css";
+import Map from "@/components/MapLeaflet";
+import "@/App.css";
 import { useState } from "react";
-import type { Floor } from "./types/MapTypes";
+import { type MapName, type Floor } from "@/types/MapTypes";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogOverlay,
+    DialogPortal,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import SelectMapDialog from "./components/SelectMapDialog";
 
 function App() {
+    const [mapName, setMapName] = useState<MapName>("chalet");
     const [floor, setFloor] = useState<Floor>("basement");
 
     return (
         <>
-            <div className="flex flex-row gap-2">
-                <button
-                    className="cursor-pointer"
-                    onClick={() => setFloor("basement")}
-                >
-                    Basement
-                </button>
-                <button
-                    className="cursor-pointer"
-                    onClick={() => setFloor("first")}
-                >
-                    First
-                </button>
-                <button
-                    className="cursor-pointer"
-                    onClick={() => setFloor("second")}
-                >
-                    Second
-                </button>
-                <button
-                    className="cursor-pointer"
-                    onClick={() => setFloor("roof")}
-                >
-                    Roof
-                </button>
-            </div>
-            <Map mapName="chalet" floor={floor} />
+            <SelectMapDialog onMapSelected={setMapName} />
+            <Map mapName={mapName} floor={floor} />
         </>
     );
 }
